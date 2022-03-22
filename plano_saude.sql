beb52803956f31fe9dfc8c71f0def63ebd02257f
sti <sti@matera.com>
2022-03-22 16:21:28 -0300

    Subindo mais uma cliente para testar dependentes

--- plano_saude.sql
+++ plano_saude.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE plano_saude; 
 
+
 CREATE TABLE cliente (id_cliente serial CONSTRAINT pk_id_cliente PRIMARY KEY NOT NULL,
                     nome varchar(255) NOT NULL,
                     dt_nasc date NOT NULL, 
@@ -12,4 +13,12 @@ INSERT INTO cliente (nome,
                                     '07-04-1995',
                                     'hannahcassia@gmail.com');
 
+INSERT INTO cliente (nome,
+                    dt_nasc,
+                    email) VALUES ('Beatriz',
+                                    '28-05-1999',
+                                    'beatrizmirandaliv@outlook.com');
+
 SELECT * FROM cliente; 
+
+

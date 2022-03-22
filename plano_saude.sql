1783ca90f4ffb63724e5bcc4ca5b5d4243963b71
sti <sti@matera.com>
2022-03-22 12:31:16 -0300

    Banco de dados criada mais tabela de cliente

---
+++ plano_saude.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE plano_saude; 
+
+CREATE TABLE cliente (id_cliente serial CONSTRAINT pk_id_cliente PRIMARY KEY NOT NULL,
+                    nome varchar(255) NOT NULL,
+                    dt_nasc date NOT NULL, 
+                    email varchar(255) NOT NULL);
+
+
+INSERT INTO cliente (nome,
+                    dt_nasc,
+                    email) VALUES ('Hannah',
+                                    '07-04-1995',
+                                    'hannahcassia@gmail.com');
+
+SELECT * FROM cliente; 
